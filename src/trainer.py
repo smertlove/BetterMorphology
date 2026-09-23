0c87e7d015d3ebfa901d4c2d24d2ab1d761d0386
Kirill Soloshenko <k.soloshenko1@mts.ai>
2026-09-23 17:01:48 +0300

    add sharding

--- src/trainer.py
+++ src/trainer.py
@@ -255,7 +255,7 @@ class MultitaskTrainer:
             batch_size=batch_size,
             collate_fn=collate_fn,
             worker_init_fn=worker_init_fn,
-            num_workers=1,
+            num_workers=4,
         )
 
         val_dataloader = DataLoader(
@@ -264,7 +264,7 @@ class MultitaskTrainer:
             shuffle=False,
             collate_fn=collate_fn,
             worker_init_fn=worker_init_fn,
-            num_workers=1,
+            num_workers=4,
         )
 
         all_metrics = self._run_training_loop(
